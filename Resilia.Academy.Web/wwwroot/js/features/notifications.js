9ce1a91f653a68fb65167a545c8692f1ddd2289a
Jorge Tinoco <jorgel.tinoco@gmail.com>
2023-01-24 17:16:56 -0600

    feat: implement toast notifications with bootstrap

---
+++ Resilia.Academy.Web/wwwroot/js/features/notifications.js
@@ -0,0 +1,15 @@
+﻿
+/**
+ * Shows a toast notification.
+ * @param {int} id - notification id
+ * @param {string} title - notification title
+ * @param {string} time - how long ago the notification was generated.
+ * @param {string} body - notification text body.
+ */
+function ShowNotification(id,title, time, body) {
+	$("#toast-template").clone().prop('id', id).appendTo("#notification-space");
+	$(`#${id} .notification-title`).html(title);
+	$(`#${id} .notification-time`).html(time);
+	$(`#${id} .notification-body`).html(body);
+	$(`#${id}`).toast('show');
+}
