a3928be843895509911b3acd8e118161c702056c
Jorge Tinoco <jorgel.tinoco@gmail.com>
2023-01-24 18:28:44 -0600

    feat: incluse API skeleton

---
+++ Resilia.Academy.Api.Models/NotificationModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Resilia.Academy.Api.Models
+{
+    /// <summary>
+    /// Represents a single notification.
+    /// </summary>
+    public class NotificationModel
+    {
+        /// <summary>
+        /// Notification title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Notification message
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// When the nofitication was generated.
+        /// </summary>
+        public string TimeAgo { get; set; }
+    }
+}
---
+++ Resilia.Academy.Api/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resilia.Academy.Api.Models;
+
+namespace Resilia.Academy.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationController : Controller
+    {
+        private readonly ILogger<NotificationController> _logger;
+
+        public NotificationController(ILogger<NotificationController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get all the notifications for an specific user.
+        /// </summary>
+        /// <param name="user">User email.</param>
+        /// <returns>Notifications.</returns>
+        [HttpGet]
+        public IEnumerable<NotificationModel> Get(string user)
+        {
+            var fakeNotification = new NotificationModel() { Title = " Hi , there", Message = "this is notifiation", TimeAgo = "right now" };
+            var arrayNotif = new NotificationModel[1] {fakeNotification};
+            return arrayNotif;
+        }
+    }
+}
